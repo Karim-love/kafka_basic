4e20f73a93d501604ddc9fefcc1bc957ac24ce8f
Karim <limsu_@naver.com>
2021-11-01 12:00:47 +0900

    1. Consumer 기초 작성 2. logback 위치 조정

---
+++ src/main/java/kafkaConsumer/KafkaConsumerBasis.java
@@ -0,0 +1,47 @@
+package kafkaConsumer;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.Collections;
+import java.util.Properties;
+
+/**
+ * Created by sblim
+ * Date : 2021-11-01
+ * Time : 오전 10:48
+ */
+
+public class KafkaConsumerBasis {
+
+    private static final String KAFKA_SINGLE_IP = "192.168.124.238:1146";
+    private static final String TOPIC_NAME = "karim-topic";
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+
+        // kafka server host 및 port 설정
+        props.put("bootstrap.servers", KAFKA_SINGLE_IP);
+        props.put("group.id", "karim-group-id-1"); // group-id 설정
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer"); // key deserializer
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer"); // value deserializer
+
+        // consumer 생성
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        // topic 설정
+        consumer.subscribe(Collections.singletonList(TOPIC_NAME));
+
+        try {
+            while (true) {
+                // 계속 loop를 돌면서 producer의 message를 띄운다.
+                ConsumerRecords<String, String> records = consumer.poll(100);
+                for (ConsumerRecord<String, String> record : records)
+                    System.out.println(record.value());
+            }
+        } catch (Exception e) {
+        } finally {
+            consumer.close();
+        }
+    }
+}
